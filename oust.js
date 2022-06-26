ea02d668bb9f2539361b34daaf07e21c11494804
Seven Y Q Guo <superShy77@163.com>
2022-06-26 15:24:12 +0800

    support media

--- oust.js
+++ oust.js
@@ -3,10 +3,16 @@ const typeMap = {
   'stylesheets': 'link'
 };
 
-function oust(htmlString, type) {
+function oust(htmlString, type, cb) {
   const $ = cheerio.load(htmlString);
 
-  return $(typeMap[type]).get().map(item => item.attribs.href);
+  return $(typeMap[type]).get().filter((item, i) => {
+    if (typeof cb === 'function') {
+      return cb($(item), i);
+    }
+
+    return true;
+  }).map((item) => item.attribs.href);
 }
 
 module.exports = oust;
--- test/index.test.js
+++ test/index.test.js
@@ -3,8 +3,18 @@ const oust = require('../oust');
 
 test('should return an array of stylesheet link hrefs', () => {
   const htmlString = fs.readFileSync(__dirname + '/stylesheet.html', 'utf-8');
-  const expected = oust(htmlString, 'stylesheets');
+  const expected = ['assets/css/bootstrap.css', 'styles/main.css'];
   const links = oust(htmlString, 'stylesheets');
+
+  expect(links).toEqual(expected);
+});
+
+test('should return an array of stylesheet link hrefs', () => {
+  const htmlString = fs.readFileSync(__dirname + '/media.html', 'utf-8');
+  const expected = ['styles/print.css'];
+  const links = oust(htmlString, 'stylesheets', ($el, i) => {
+    return $el.attr('media') === 'print';
+  });
 
   expect(links).toEqual(expected);
 });
