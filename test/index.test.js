5da7935b7a309f7232602bb4a4db021b98a3377c
Seven Y Q Guo <superShy77@163.com>
2022-06-26 15:52:02 +0800

    meaningful name

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const oust = require('../oust');
 
 test('should return an array of stylesheet link hrefs', () => {
-  const htmlString = fs.readFileSync(__dirname + '/stylesheet.html', 'utf-8');
+  const htmlString = fs.readFileSync(__dirname + '/stylesheets.html', 'utf-8');
   const expected = ['assets/css/bootstrap.css', 'styles/main.css'];
   const links = oust(htmlString, 'stylesheets');
 
@@ -10,10 +10,30 @@ test('should return an array of stylesheet link hrefs', () => {
 });
 
 test('should return an array of stylesheet link hrefs via condition', () => {
-  const htmlString = fs.readFileSync(__dirname + '/media.html', 'utf-8');
+  const htmlString = fs.readFileSync(__dirname + '/stylesheets-media.html', 'utf-8');
   const expected = ['styles/print.css'];
   const links = oust(htmlString, 'stylesheets', ($el, i) => {
     return $el.attr('media') === 'print';
+  });
+
+  expect(links).toEqual(expected);
+});
+
+test('should return an array of HTML imports', () => {
+  const htmlString = fs.readFileSync(__dirname + '/stylesheets-import.html', 'utf-8');
+  const expected = ['../polymer/polymer.html', '../core-ajax/core-ajax.html', '../core-input/core-input.html'];
+  const links = oust(htmlString, 'stylesheets', ($el, i) => {
+    return $el.attr('rel') === 'import';
+  });
+
+  expect(links).toEqual(expected);
+});
+
+test('should return an array of stylesheet preload hrefs', () => {
+  const htmlString = fs.readFileSync(__dirname + '/stylesheets-preload.html', 'utf-8');
+  const expected = ['styles/preload.css'];
+  const links = oust(htmlString, 'stylesheets', ($el, i) => {
+    return $el.attr('rel') === 'preload';
   });
 
   expect(links).toEqual(expected);
@@ -27,28 +47,8 @@ test('should return an array of script srcs', () => {
   expect(links).toEqual(expected);
 });
 
-test('should return an array of HTML imports', () => {
-  const htmlString = fs.readFileSync(__dirname + '/imports.html', 'utf-8');
-  const expected = ['../polymer/polymer.html', '../core-ajax/core-ajax.html', '../core-input/core-input.html'];
-  const links = oust(htmlString, 'stylesheets', ($el, i) => {
-    return $el.attr('rel') === 'import';
-  });
-
-  expect(links).toEqual(expected);
-});
-
-test('should return an array of stylesheet preload hrefs', () => {
-  const htmlString = fs.readFileSync(__dirname + '/preload.html', 'utf-8');
-  const expected = ['styles/preload.css'];
-  const links = oust(htmlString, 'stylesheets', ($el, i) => {
-    return $el.attr('rel') === 'preload';
-  });
-
-  expect(links).toEqual(expected);
-});
-
-test('should return an array of stylesheet preload hrefs', () => {
-  const htmlString = fs.readFileSync(__dirname + '/preload.html', 'utf-8');
+test('should return an array of link URLs', () => {
+  const htmlString = fs.readFileSync(__dirname + '/links.html', 'utf-8');
   const expected = ['index.html', 'about.html', 'contact.html', '#'];
   const links = oust(htmlString, 'links');
 
