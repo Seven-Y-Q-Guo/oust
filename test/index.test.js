26c557cc295fb34c621a91fa7eb4f10176bc138a
Seven Y Q Guo <superShy77@163.com>
2022-06-26 15:09:08 +0800

    test data as file

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,10 @@
+const fs = require('fs');
 const oust = require('../oust');
-const htmlString = `<style>body {padding:0}</style>
-<link rel="stylesheet preload" href="assets/css/bootstrap.css" as="style" />
-<style>h1 {font-size: 5rem}</style>
-<link rel="stylesheet" href="styles/main.css">`;
 
-test('get stylesheets', () => {
-  expect(oust(htmlString, 'stylesheets')).toEqual(['assets/css/bootstrap.css', 'styles/main.css']);
+test('should return an array of stylesheet link hrefs', () => {
+  const htmlString = fs.readFileSync(__dirname + '/stylesheet.html', 'utf-8');
+  const expected = oust(htmlString, 'stylesheets');
+  const links = oust(htmlString, 'stylesheets');
+
+  expect(links).toEqual(expected);
 });
