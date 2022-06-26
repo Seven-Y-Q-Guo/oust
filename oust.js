4a6e84b0b5e579e2be84b12bf89107ea6eccb742
Seven Y Q Guo <superShy77@163.com>
2022-06-26 16:46:11 +0800

    use reduce instead

--- oust.js
+++ oust.js
@@ -24,20 +24,17 @@ const typeMap = {
 
 function oust(htmlString, type, cb) {
   const $ = cheerio.load(htmlString);
-  let results = [];
   const arr = typeof type === 'string' ? [type] : type;
 
-  arr.forEach((typeItem, i) => {
-    results = results.concat($(typeMap[typeItem].name).get().filter((item, i) => {
+  return arr.reduce((previousValue, currentValue) => (
+    previousValue.concat($(typeMap[currentValue].name).get().filter((item, i) => {
       if (typeof cb === 'function') {
         return cb($(item), i);
       }
 
       return true;
-    }).map((item) => typeMap[typeItem].children ? item.children[0].data : item.attribs[typeMap[typeItem].attr]));
-  });
-
-  return results;
+    }).map((item) => typeMap[currentValue].children ? item.children[0].data : item.attribs[typeMap[currentValue].attr]))
+  ), []);
 }
 
 module.exports = oust;
